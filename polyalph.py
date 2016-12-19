2d6900624076fa2f649a3d614b06eedfdf17f6db
Bora Turan <boraturan1@gmail.com>
2016-12-19 10:31:50 +0300

    Working both ways

--- polyalph.py
+++ polyalph.py
@@ -1,3 +1,15 @@
+lang = input("""Ingilicce: EN
+             Turkce: TR
+             Kendi alfabeni tanimlamak icin: CUS
+             giriniz\n""")
+if lang == "EN":
+    alph = "abcdefghijklmnopqrstuvwxyz"
+elif lang == "TR":
+    alph = "abcçdefgğhıijklmnoöprsştuüvyz"
+if lang == "CUS":
+    alph = input("Alfabenizi giriniz:\n")
+
+
 def append_indices(indices):
     ext_indices = indices
     step = 0
@@ -8,20 +20,25 @@ def append_indices(indices):
         step += 1
     return extended_indices
 
+
 def make_alph_range(indices): #or index if it's the latter case
     if type(indices) is list:
-        return [index - (index // 26) * 26 if index >= 26 else index for index in indices]
+        return [index - (index // len(alph)) * len(alph) if index >= len(alph) else index + len(alph) if index <= 0 else index for index in indices]
     elif type(indices) is int:
         index = indices
-        if index >= 26:
-            return index - (index // 26) * 26
+        if index >= len(alph):
+            return index - (index // len(alph)) * len(alph)
         else: return index
 
 
-def shift(word, key):
-    alph = "abcdefghijklmnopqrstuvwxyz"
+
+def shift(word, key, way):
     key_indices = [alph.index(char) for char in key]
     word_indices = [alph.index(char) for char in word]
+    for i in range(len(key_indices)):
+        key_indices[i] += 1
+    for i in range(len(word_indices)):
+        word_indices[i] += 1
     multipler = len(word) // len(key) + 1
     while multipler > 0:
         append_indices(key_indices)
@@ -29,10 +46,14 @@ def shift(word, key):
 
     crypto = []
     key_index = 0
-    for word_index in word_indices:
-         crypto.append(alph[make_alph_range(key_indices[key_index] + word_index + 1)])
-         key_index += 1
-    print(crypto)
+    if way is True:
+        for word_index in word_indices:
+             crypto.append(alph[make_alph_range(key_indices[key_index] + word_index - 1)])
+             key_index += 1
+    elif way is False:
+        for word_index in word_indices:
+            crypto.append(alph[make_alph_range(word_index - key_indices[key_index] - 1)])
+            key_index += 1
     return crypto
 
 
@@ -56,15 +77,22 @@ def isalpha(word):
     return not has_num
 
 print("""Polialabetik Sifreleme:
-Lutfen bir kelime ve bir anahtar kelime saglayiniz.\n""")
+    Sifreleme yonu belirtiniz.\n""")
+
+while True:
+    try:
+        way = bool(int(input("""Sifrelemek icin herhangi bir sayiya,
+                             Sifre cozmek icin 0a basiniz: """)))
+        break
+    except ValueError:
+        print("Lutfen sadece sayi giriniz!\n")
 
 key = get_word("Anahtar Kelime: ")
-
 results = []
 go_on = True
 while go_on is True:
     word = get_word("Kelime: ")
-    results.append(shift(word, key))
+    results.append(shift(word, key, way))
     while True:
         try:
             go_on = bool(int(input("Devam etmek icin herhangi bir sayiya,\nDurmak icin 0a basiniz: ")))
